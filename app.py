faae847735efe7d7adb1815e32b8e649c2c1146d
saihaneesh26 <56464193+saihaneesh26@users.noreply.github.com>
2022-06-07 21:40:25 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,37 @@
+import json
+from flask import Flask, render_template, request
+import pandas as pd
+
+app = Flask(__name__,template_folder="templates")
+@app.route('/',methods=['POST','GET'])
+def mainPage():
+    if request.method=="POST":
+        print(int(request.form.get("index")))
+    url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTo9pcOAqd_-2v6taNNfEIMOsjJQ0pnDsPslujwdWbvKocIWhvfVVXnfL38kzq8-E-9oQVYvyZpRYMV/pub?output=csv'
+    df = pd.read_csv(url).to_json(orient="records")
+    return render_template("index.html",df=df)
+
+    
+@app.route('/view',methods=['POST','GET'])
+def viewPage():
+    if request.method=="POST":
+        try:
+            index = (request.form.get("link"))
+            url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTo9pcOAqd_-2v6taNNfEIMOsjJQ0pnDsPslujwdWbvKocIWhvfVVXnfL38kzq8-E-9oQVYvyZpRYMV/pub?output=csv'
+            df = pd.read_csv(url)
+            if index=="-1":
+                pageurl = df['Upload file'][0]
+                print(pageurl)
+            else:
+                pageurl = index.split('id=')[1]
+                pageurl = "https://drive.google.com/file/d/"+pageurl+"/preview allow='autoplay'"
+            return render_template("view.html",pageurl=pageurl)
+        except:
+            return render_template("index.html")
+    else:
+        return render_template("view.html")
+
+if __name__=='__main__':
+    app.run(debug=True)
+
+
